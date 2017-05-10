4450578dfa3ec973d7c00c9f1e474316e7383124
ghobot <greg@sciencelifeny.com>
2017-05-10 00:29:38 -0400

    add cube markers; add greg and jake models; add previous next buttons

---
+++ js/slidedeck_component.js
@@ -0,0 +1,36 @@
+AFRAME.registerComponent('slidedeck', {
+    init: function () {
+       
+
+
+        // const spotHeight = 3;
+        // const slideSpots = [
+
+        //     /* South hallway */
+        //     {x: 1, y: spotHeight, z: -4},
+        //     {x: 1, y: spotHeight, z: -6.7},
+        //     {x: 1, y: spotHeight, z: -9.4},
+        //     {x: 1, y: spotHeight, z: -12.1},
+
+        //     {x: 0, y: spotHeight, z: -13},
+
+        //     {x: -1, y: spotHeight, z: -4},
+        //     {x: -1, y: spotHeight, z: -6.7},
+        //     {x: -1, y: spotHeight, z: -9.4},
+        //     {x: -1, y: spotHeight, z: -12.1}
+        // ]; 
+        console.log("slidedeck started");
+    },
+
+    update: function () {
+
+    },
+
+    tick: function () {
+
+    },
+
+    remove: function () {
+
+    }
+});
--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 var startExperienteBtn = document.getElementById('start_experience');
 
-startExperienteBtn.onclick = function(){
+startExperienteBtn.onclick = function () {
     document.getElementById('container').outerHTML = '';
-    document.getElementsByTagName('a-scene')[0].style.zIndex = 'auto';
+    document.getElementsByTagName('a-scene')[0].style.zIndex = '1';
+
+     /* Add slidedeck controls if on desktop*/
+    if ( !AFRAME.utils.checkHeadsetConnected() ) {
+        var buttonElPrevious = document.createElement('button'), buttonElNext = document.createElement('button');
+        var nextText = "Next", previousText = "Previous";
+        buttonElPrevious.className += "slideButton";
+        buttonElNext.className += "slideButton";
+        buttonElPrevious.innerHTML = previousText;
+        buttonElNext.innerHTML = nextText;
+
+        var wrapper = document.getElementById("presentation-nav");
+        wrapper.appendChild(buttonElPrevious);
+        wrapper.appendChild(buttonElNext);
+
+        document.body.appendChild(wrapper);
+    }
 };
