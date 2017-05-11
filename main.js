5c9ad338140fcc9d15a2f0d838eb85f129fbb3e0
ghobot <greg@sciencelifeny.com>
2017-05-11 08:48:49 -0400

    add deck button navigation; add link to google slides

--- main.js
+++ main.js
@@ -16,38 +16,60 @@ startExperienteBtn.onclick = function () {
         buttonElNext.innerHTML = nextText;
 
         var wrapper = document.getElementById("presentation-nav");
-        wrapper.appendChild(buttonElPrevious);
+        //wrapper.appendChild(buttonElPrevious);
         wrapper.appendChild(buttonElNext);
 
         document.body.appendChild(wrapper);
 
-        const spotHeight = 3;
-        const slideSpots = [
+        var camHeight = 1.600;
+        var slidePositions = [
 
-            /* South hallway */
-            {x: 1, y: spotHeight, z: -4},
-            {x: 1, y: spotHeight, z: -6.7},
-            {x: 1, y: spotHeight, z: -9.4},
-            {x: 1, y: spotHeight, z: -12.1},
-
-            {x: 0, y: spotHeight, z: -13},
-
-            {x: -1, y: spotHeight, z: -4},
-            {x: -1, y: spotHeight, z: -6.7},
-            {x: -1, y: spotHeight, z: -9.4},
-            {x: -1, y: spotHeight, z: -12.1}
+            
+            {x: -4.600 , y: camHeight, z: 0.264},
+            {x: -5.536 , y: camHeight, z: 0},
+            {x: -0.900 , y: camHeight, z: 3.931},
+            {x: -0.900 , y: camHeight, z: 6.970},
+            
         ]; 
+
+        var slideRotations = [
+            {x:-3.209 , y:0 , z:0},
+            {x:0 , y:90 , z:0},
+            {x:0 , y:90 , z:0},
+            {x:0 , y:90 , z:0},
+            
+        ]
 
         console.log("slidedeck started");
 
+        var spotNum = slidePositions.length;
+
         buttonElNext.onclick = function () {
-        	currentLoc++;
-        	if (currentLoc > slideSpots.length() ) {
-        		currentLoc = 0;
-        	}
-        	//e.setAttribute("position",slideSpots[currentLoc]);
-        	console.log("current location: " + currentLoc);
+        	
+            document.querySelector('a-camera').setAttribute('position' , slidePositions[currentLoc]);
+            document.querySelector('a-camera').setAttribute('rotation' , slideRotations[currentLoc]);
+            currentLoc++;
+
+            if (currentLoc == spotNum) {
+                currentLoc = 0;
+            }
+
+            console.log("currentloc = " + currentLoc);
+        	
         };
+        // buttonElPrevious.onclick = function () {
+            
+        //     currentLoc--;
+        //     document.querySelector('a-camera').setAttribute('position' , slidePositions[currentLoc]);
+        //     document.querySelector('a-camera').setAttribute('rotation' , slideRotations[currentLoc]);
+            
+
+        //     if (currentLoc < 0) {
+        //         currentLoc = 0;
+        //     }
+        //     console.log("currentloc = " + currentLoc);
+            
+        // };
 
     }
 };
